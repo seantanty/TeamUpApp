ab5cdc1bf4be161ec8e91ee553cf7ccddc7c979d
seantanty <seantanty@gmail.com>
2021-03-29 22:47:10 -0700

    db added
    previous project code added for reference, use db's project3 collection

---
+++ db/MyDB.js
@@ -0,0 +1,296 @@
+const { MongoClient, ObjectId } = require("mongodb");
+
+function MyDB() {
+  const myDB = {};
+
+  // const url = process.env.MONGODB_URI;
+  const url =
+    "mongodb+srv://seantan:password@cluster0.u90qt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const DB_NAME = "5610Project3";
+
+  //below code are from previous project, use as reference
+
+  // Get popular puzzles for displaying leader boards.
+  myDB.getPuzzles = async () => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true });
+      await client.connect();
+      const db = client.db(DB_NAME);
+      const filesCol = db.collection("puzzles");
+      const files = await filesCol
+        .aggregate([
+          {
+            $project: {
+              leaderBoard: 1,
+              length: { $size: "$usersPlayed" },
+              code: 1,
+            },
+          },
+          { $sort: { length: -1 } },
+          { $limit: 3 },
+        ])
+        .toArray();
+      return files;
+    } finally {
+      client.close();
+    }
+  };
+
+  // get the puzzle searched by puzzle id
+  myDB.getPuzzleById = async (query) => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true });
+      await client.connect();
+      const db = client.db(DB_NAME);
+      const filesCol = db.collection("puzzles");
+      const files = await filesCol.find({ code: query }).toArray();
+      return files[0];
+    } finally {
+      client.close();
+    }
+  };
+
+  // get three puzzles for each size for playing
+  myDB.getPuzzleBySize = async () => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true });
+      await client.connect();
+      const db = client.db(DB_NAME);
+      const filesCol = db.collection("puzzles");
+      const files = new Array(3);
+      var sizes = new Array(5, 10, 15);
+      for (var i = 0; i < 3; i++) {
+        const file = await filesCol
+          .aggregate([{ $match: { size: sizes[i] } }, { $sample: { size: 1 } }])
+          .toArray();
+        files[i] = file;
+      }
+
+      return files;
+    } finally {
+      client.close();
+    }
+  };
+
+  //user register
+  myDB.createUser = async (user) => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true });
+      await client.connect();
+      const db = client.db(DB_NAME);
+      const usersCol = db.collection("Users");
+      const existed = await usersCol.findOne({ username: user.username });
+      if (existed != null) {
+        return null;
+      } else {
+        const res = await usersCol.insertOne(user);
+        return res;
+      }
+    } finally {
+      client.close();
+    }
+  };
+
+  //check if same username exist
+  myDB.findSameUserName = async (query = {}) => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true });
+      await client.connect();
+      const db = client.db(DB_NAME);
+      const userCol = db.collection("Users");
+      const data = await userCol.find(query).toArray();
+
+      if (data != null && data.length == 1) {
+        if (data[0].username === query.username) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    } finally {
+      client.close();
+    }
+  };
+
+  myDB.findUserByUserName = async (query = {}, done) => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true });
+      await client.connect();
+      const db = client.db(DB_NAME);
+      const userCol = db.collection("Users");
+      const data = await userCol.find(query).toArray();
+
+      if (data != null && data.length == 1) {
+        return done(null, data[0]);
+      } else {
+        return done(null, null);
+      }
+    } finally {
+      client.close();
+    }
+  };
+
+  myDB.findUserById = async (query = {}, done) => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true });
+      await client.connect();
+      const db = client.db(DB_NAME);
+      const userCol = db.collection("Users");
+      const data = await userCol.find(query).toArray();
+
+      if (data != null && data.length == 1) {
+        return done(null, data[0]);
+      } else {
+        return done(null, null);
+      }
+    } finally {
+      client.close();
+    }
+  };
+
+  //save time to leaderboard if the time is in top 10.
+  //leaderboard only maintain top 10(shortest) time with its user
+  myDB.saveToLeaderBoard = async (query = {}) => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true });
+      await client.connect();
+      const db = client.db(DB_NAME);
+      const puzzleCol = db.collection("puzzles");
+      let o_id = new ObjectId(query.puzzleId);
+      const data = await puzzleCol.find({ _id: o_id }).toArray();
+
+      let res = null;
+      let dbLb = data[0].leaderBoard;
+
+      if (query.index == 11 || dbLb.length == undefined) {
+        res = await db.collection("puzzles").updateOne(
+          { _id: data[0]._id },
+          {
+            $push: { leaderBoard: { 0: query.username, 1: query.time } },
+          }
+        );
+      } else {
+        if (query.trim == true) {
+          let pos = query.index;
+          res = await db.collection("puzzles").updateOne(
+            { _id: data[0]._id },
+            {
+              $push: {
+                leaderBoard: {
+                  $each: [{ 0: query.username, 1: query.time }],
+                  $position: pos,
+                },
+              },
+            }
+          );
+          res = await db.collection("puzzles").updateOne(
+            { _id: data[0]._id },
+            {
+              $pop: {
+                leaderBoard: 1,
+              },
+            }
+          );
+        } else {
+          let pos = query.index;
+          res = await db.collection("puzzles").updateOne(
+            { _id: data[0]._id },
+            {
+              $push: {
+                leaderBoard: {
+                  $each: [{ 0: query.username, 1: query.time }],
+                  $position: pos,
+                },
+              },
+            }
+          );
+        }
+      }
+      return res;
+    } finally {
+      client.close();
+    }
+  };
+
+  //save played puzzle time to user only if the time is better then previous time
+  myDB.saveTimeToUser = async (query = {}) => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true });
+      await client.connect();
+      const db = client.db(DB_NAME);
+      const userCol = db.collection("Users");
+      let findUserQuery = { username: query.username };
+      const data = await userCol.find(findUserQuery).toArray();
+
+      let res = { result: "No need to update" };
+      let games = data[0].played;
+
+      //if time is better then current or there is no time yet
+      if (games.length == 0) {
+        //add puzzleId with time to array
+        res = await db.collection("Users").updateOne(
+          { _id: data[0]._id },
+          {
+            $push: { played: { gameId: query.puzzleCode, time: query.time } },
+          }
+        );
+      } else {
+        let index = null;
+        for (let i = 0; i < games.length; i++) {
+          if (games[i].gameId == query.puzzleCode) {
+            index = i;
+            break;
+          }
+        }
+        if (games.length == undefined) {
+          res = await db.collection("Users").updateOne(
+            { _id: data[0]._id },
+            {
+              $push: { played: { gameId: query.puzzleCode, time: query.time } },
+            }
+          );
+        } else if (index != null) {
+          if (query.time < games[index].time) {
+            //replace existing puzzleId's time
+            let updateQuery = {
+              _id: data[0]._id,
+              "played.gameId": query.puzzleCode,
+            };
+            let updateDoc = {
+              $set: { "played.$.time": query.time },
+            };
+            res = await db
+              .collection("Users")
+              .updateOne(updateQuery, updateDoc);
+          }
+        } else if (index == null) {
+          //first time play game, add puzzleId with time to array
+          res = await db.collection("Users").updateOne(
+            { _id: data[0]._id },
+            {
+              $push: { played: { gameId: query.puzzleCode, time: query.time } },
+            }
+          );
+        }
+      }
+      return res;
+    } finally {
+      client.close();
+    }
+  };
+
+  return myDB;
+}
+
+module.exports = MyDB();
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,175 @@
 var express = require("express");
 var router = express.Router();
+var path = require("path");
+var passport = require("passport");
+var bcrypt = require("bcrypt");
+const myDB = require("../db/MyDB.js");
+const saltRounds = 10;
 
-/* GET home page. */
+/* previous project code for reference */
+//middle function to check login status before show profile page
+function loggedIn(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+}
+
+//index GET
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/../public/index.html"));
+});
+
+//register GET
+router.get("/register", function (req, res) {
+  res.sendFile(path.join(__dirname + "/../public/register.html"));
+});
+
+//check same user name before register
+router.post("/checkSameUserName", async (req, res) => {
+  try {
+    const result = await myDB.findSameUserName(req.body);
+    res.send({ same: result });
+  } catch (e) {
+    console.log("Error", e);
+    res.status(400).send({ err: e });
+  }
+});
+
+//register POST
+router.post("/register", async (req, res) => {
+  try {
+    const hashedPwd = await bcrypt.hash(req.body.password, saltRounds);
+    const userObj = {
+      username: req.body.username,
+      password: hashedPwd,
+      played: [],
+    };
+    const dbRes = await myDB.createUser(userObj);
+    if (dbRes == null) {
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/");
+      });
+    }
+  } catch (e) {
+    console.log("Error", e);
+    res.status(400).send({ err: e });
+  }
+});
+
+//login GET
+router.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname + "/../public/login.html"));
+});
+
+//login POST
+router.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/login" }),
+  function (req, res) {
+    console.log("successful login");
+    res.redirect("/");
+  }
+);
+
+//logout GET
+router.get("/logout", loggedIn, function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+//route to get user info
+router.get("/getUser", (req, res) =>
+  res.send({
+    username: req.user ? req.user.username : null,
+    played: req.user ? req.user.played : null,
+  })
+);
+
+//profile GET
+router.get("/profile", loggedIn, function (req, res) {
+  res.sendFile(path.join(__dirname + "/../public/profile.html"));
+});
+
+/*index routs*/
+// get puzzle by size
+router.get("/getPuzzlesPlay", async (req, res) => {
+  try {
+    console.log("Getting puzzles by size");
+    const puzzles = await myDB.getPuzzleBySize();
+    res.send({ puzzles: puzzles });
+  } catch (e) {
+    console.log("Error", e);
+    res.status(400).send({ err: e });
+  }
+});
+
+/*leader board routes*/
+// get boards of popular puzzles
+router.get("/getPuzzles", async (req, res) => {
+  try {
+    console.log("Getting polular puzzles");
+    const puzzles = await myDB.getPuzzles();
+    res.send({ puzzles: puzzles });
+  } catch (e) {
+    console.log("Error", e);
+    res.status(400).send({ err: e });
+  }
+});
+
+//get board by searching puzzle id
+router.post("/searchBoard", async (req, res) => {
+  try {
+    const puzzleId = req.body.puzzleid;
+    const puzzle = await myDB.getPuzzleById(puzzleId);
+    let leaderBoard = {
+      success: false,
+    };
+    if (puzzle == null) {
+      console.log("No leaderboard found");
+    } else {
+      leaderBoard = {
+        code: puzzle.code,
+        leaderBoard: puzzle.leaderBoard,
+        info: puzzle.info,
+        success: true,
+      };
+    }
+    res.send(leaderBoard);
+  } catch (e) {
+    console.log("Error", e);
+    res.status(400).send({ err: e });
+  }
+});
+
+//route to save solved puzzle to user collection
+router.post("/saveTimeToUser", async (req, res) => {
+  try {
+    const saveTime = await myDB.saveTimeToUser(req.body);
+    res.send(saveTime);
+  } catch (e) {
+    console.log("Error", e);
+    res.status(400).send({ err: e });
+  }
+});
+
+//route to save solved puzzle to puzzle collection
+router.post("/saveToLeaderBoard", async (req, res) => {
+  try {
+    const saveLB = await myDB.saveToLeaderBoard(req.body);
+    res.send(saveLB);
+  } catch (e) {
+    console.log("Error", e);
+    res.status(400).send({ err: e });
+  }
+});
+
+/* previous project code for reference */
+
+/* defualt file setup, pending delete */
 router.get("/", function (req, res) {
   res.render("index", { title: "Express" });
 });
