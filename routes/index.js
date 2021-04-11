8f71b3dbe1dce7d20504ecfb7f5290545d405e30
XinyiJackieGe <61487236+XinyiJackieGe@users.noreply.github.com>
2021-04-11 05:01:48 -0700

    fix login post

--- routes/index.js
+++ routes/index.js
@@ -130,7 +130,9 @@ router.post(
   passport.authenticate("local", { failureRedirect: "/login" }),
   function (req, res) {
     console.log("successful login");
-    res.redirect("/");
+    console.log("userid", req.user._id);
+    res.send({userid: req.user._id});
+    //res.redirect("/");
   }
 );
 
